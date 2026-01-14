d27dd85a5b0a5e82e0fef1dbf02b9d5b2d2125e2
Shailesh <shaileshhawale004@gmail.com>
2026-01-14 19:02:19 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets'   # To write to sheets
 ]
 
-# Create a new Google Sheet manually and paste its ID here
-# Example URL: https://docs.google.com/spreadsheets/d/abc1234567.../edit
-SPREADSHEET_ID = '1sX0m2HlJeAXXmoK_cSN_G9VKQeaXpgc1kzDNQI0x_ao' 
+
+SPREADSHEET_ID = 'PUT SPREADSHEET ID HERE' 
 RANGE_NAME = 'Sheet1!A:D' # Columns A to D
 CREDENTIALS_FILE = 'credentials/credentials.json'
 TOKEN_FILE = 'credentials/token.json'
